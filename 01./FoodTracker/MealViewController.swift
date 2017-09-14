59ec31750e85e6defe4f3f612a876ed3e5705131
Kenneth Kang <kangjor@gmail.com>
2017-09-14 14:52:54 +0900

    done editing and deleting item example

--- 01./FoodTracker/MealViewController.swift
+++ 01./FoodTracker/MealViewController.swift
@@ -30,6 +30,15 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         // Do any additional setup after loading the view, typically from a nib.
         nameTextField.delegate = self
         updateSaveButtonState()
+        
+        // Set up views if editing an existing meal.
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
     }
 
 
@@ -58,8 +67,16 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         meal = Meal(name: name, photo: photo, rating: rating)
     }
     
+    // dismiss view and back to the previous view
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        // !!!!! important
+        // Depending on style of presentation (modal or push presentation), this view controller need to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        } else if let owningNavigationController = navigationController {
+            owningNavigationController.popViewController(animated: true)
+        }
     }
     
     // MARK: Actions
